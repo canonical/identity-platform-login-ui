332e3ad2ac5c52c013e2b49f138ab044da6eaa13
Nikos <nikos.sklikas@canonical.com>
2023-02-02 14:58:54 +0200

    Rebase

--- main.go
+++ main.go
@@ -21,8 +21,10 @@ func main() {
 	dist, _ := fs.Sub(ui, "ui/dist")
 
 	http.Handle("/", http.FileServer(http.FS(dist)))
-	http.HandleFunc("/api/hydra", handleHydra)
-	http.HandleFunc("/api/kratos", handleKratos)
+	http.HandleFunc("/api/kratos/self-service/login/browser", handleCreateFlow)
+	http.HandleFunc("/api/kratos/self-service/login", handleUpdateFlow)
+	http.HandleFunc("/api/kratos/self-service/errors", handleKratosError)
+	http.HandleFunc("/api/consent", handleConsent)
 
 	port := os.Getenv("PORT")
 
@@ -34,12 +36,23 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func handleHydra(w http.ResponseWriter, _ *http.Request) {
-	// hydra := os.Getenv("HYDRA_ADMIN_URL")
+func handleCreateFlow(w http.ResponseWriter, _ *http.Request) {
+	// kratos := os.Getenv("KRATOS_PUBLIC_URL")
 	return
 }
 
-func handleKratos(w http.ResponseWriter, _ *http.Request) {
+func handleUpdateFlow(w http.ResponseWriter, _ *http.Request) {
 	// kratos := os.Getenv("KRATOS_PUBLIC_URL")
+	return
+}
+
+func handleKratosError(w http.ResponseWriter, _ *http.Request) {
+	// kratos := os.Getenv("KRATOS_PUBLIC_URL")
+	return
+}
+
+func handleConsent(w http.ResponseWriter, _ *http.Request) {
+	// kratos := os.Getenv("KRATOS_PUBLIC_URL")
+	// hydra := os.Getenv("HYDRA_ADMIN_URL")
 	return
 }
