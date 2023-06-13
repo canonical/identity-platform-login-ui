61fbdf202b7349cee1a2fc40b3e5dbbe8715df2f
shipperizer <alexcabb@gmail.com>
2023-06-13 16:34:22 +0100

    feat: add git commit sha on health endpoint

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -3,18 +3,58 @@ package health
 import (
 	"encoding/json"
 	"net/http"
+	"runtime/debug"
 )
 
 const okValue = "ok"
 
 type Status struct {
-	Status string `json:"status"`
+	Status    string     `json:"status"`
+	BuildInfo *BuildInfo `json:"buildInfo"`
+}
+
+type BuildInfo struct {
+	Version string `json:"version"`
+	Name    string `json:"name"`
+}
+
+func buildInfo() *BuildInfo {
+	info, ok := debug.ReadBuildInfo()
+
+	if !ok {
+		return nil
+	}
+
+	buildInfo := new(BuildInfo)
+	buildInfo.Name = info.Main.Path
+	buildInfo.Version = gitRevision(info.Settings)
+
+	return buildInfo
+}
+
+func gitRevision(settings []debug.BuildSetting) string {
+	for _, setting := range settings {
+		if setting.Key == "vcs.revision" {
+			return setting.Value
+		}
+	}
+
+	return "n/a"
 }
 
 func HandleAlive(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	body, _ := json.Marshal(Status{Status: okValue})
-	w.Write(body)
+
+	rr := Status{
+		Status: okValue,
+	}
+
+	if buildInfo := buildInfo(); buildInfo != nil {
+		rr.BuildInfo = buildInfo
+	}
+
+	json.NewEncoder(w).Encode(rr)
+
 	return
 }
