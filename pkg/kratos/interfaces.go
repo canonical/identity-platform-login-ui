0f4e552c05fde1eff5cd752d448a5bb17bf20e22
shipperizer <alexcabb@gmail.com>
2023-06-14 15:00:59 +0100

    feat: move kratos api set in a separate pkgs

---
+++ pkg/kratos/interfaces.go
@@ -0,0 +1,14 @@
+package kratos
+
+import (
+	hydra_client "github.com/ory/hydra-client-go/v2"
+	kratos_client "github.com/ory/kratos-client-go"
+)
+
+type KratosClientInterface interface {
+	FrontendApi() kratos_client.FrontendApi
+}
+
+type HydraClientInterface interface {
+	OAuth2Api() hydra_client.OAuth2Api
+}
