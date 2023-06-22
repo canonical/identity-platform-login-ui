88249302b50e979ad964e0833a6977dca56400b2
David Edler <david.edler@canonical.com>
2023-06-22 13:22:48 +0200

    fix(ui) remove workaround for WebAuthn as it is unused

--- ui/components/NodeInputButton.tsx
+++ ui/components/NodeInputButton.tsx
@@ -10,21 +10,7 @@ export const NodeInputButton: FC<NodeInputProps> = ({
   disabled,
   dispatchSubmit,
 }) => {
-  // Some attributes have dynamic JavaScript - this is for example required for WebAuthn.
   const handleClick = (e: MouseEvent | FormEvent) => {
-    // This section is only used for WebAuthn. The script is loaded via a <script> node
-    // and the functions are available on the global window level. Unfortunately, there
-    // is currently no better way than executing eval / function here at this moment.
-    //
-    // Please note that we also need to prevent the default action from happening.
-    if (attributes.onclick) {
-      e.stopPropagation();
-      e.preventDefault();
-      const run = () => attributes.onclick; // todo wtf?
-      run();
-      return;
-    }
-
     void setValue(attributes.value as string).then(() => dispatchSubmit(e));
   };
 
