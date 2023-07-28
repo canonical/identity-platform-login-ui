b8b4dedb5d69d50bab645e8aed34ff8b5ee92aa9
bencekov <bence.kovacs.business@gmail.com>
2023-07-27 22:11:25 -0600

    fix: fixed server treating all misc routes as fileserver requests
    addresses and closes https://github.com/canonical/identity-platform-login-ui/issues/100

--- pkg/ui/handlers.go
+++ pkg/ui/handlers.go
@@ -1,30 +1,36 @@
 package ui
 
 import (
+	"fmt"
 	"io/fs"
 	"net/http"
 	"path"
+	"strings"
 
 	"github.com/canonical/identity-platform-login-ui/internal/logging"
 	"github.com/go-chi/chi/v5"
 )
 
+const UI = "/ui"
+
 type API struct {
 	fileServer http.Handler
-
-	logger logging.LoggerInterface
+	logger     logging.LoggerInterface
 }
 
 func (a *API) RegisterEndpoints(mux *chi.Mux) {
 	// TODO @shipperizer unsure if we deal with any POST/PUT/PATCH via js
-	mux.HandleFunc("/*", a.uiFiles)
+	mux.HandleFunc(fmt.Sprintf("%s/*", UI), a.uiFiles)
+
 }
 
 // TODO: Validate response when server error handling is implemented
 func (a *API) uiFiles(w http.ResponseWriter, r *http.Request) {
-	if ext := path.Ext(r.URL.Path); ext == "" && r.URL.Path != "/" {
-		r.URL.Path += ".html"
+	if ext := path.Ext(r.URL.Path); ext == "" {
+		r.URL.Path = fmt.Sprintf("%s.html", r.URL.Path)
 	}
+
+	r.URL.Path = strings.TrimPrefix(r.URL.Path, UI)
 
 	a.fileServer.ServeHTTP(w, r)
 }
