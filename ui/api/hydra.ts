bff64e541ded6766c9feae668fc72468f326ec97
bencekov <bence.kovacs.business@gmail.com>
2023-08-02 18:54:00 -0600

    feat: changed the route to the ui resources to /ui/*

--- ui/api/hydra.ts
+++ ui/api/hydra.ts
@@ -3,7 +3,7 @@ import { Configuration, OAuth2Api } from "@ory/client";
 const hydraAdmin = new OAuth2Api(
   new Configuration({
     // Use relative path so that this works when served in a subpath
-    basePath: "./api/hydra",
+    basePath: "../api/hydra",
     baseOptions: {
       withCredentials: true,
     },
