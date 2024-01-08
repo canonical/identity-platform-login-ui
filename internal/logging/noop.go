57b37f8b7102b4b62057f27c9648e6546e9a0e67
Nikos <nikos.sklikas@canonical.com>
2024-01-08 14:17:46 +0200

    fix: switch to zap nop logger

--- internal/logging/noop.go
+++ internal/logging/noop.go
@@ -1,19 +1,9 @@
 package logging
 
-type NoopLogger struct {
-}
+import (
+	"go.uber.org/zap"
+)
 
-func NewNoopLogger() *NoopLogger {
-	return new(NoopLogger)
+func NewNoopLogger() *zap.SugaredLogger {
+	return zap.NewNop().Sugar()
 }
-
-func (l *NoopLogger) Errorf(string, ...interface{}) {}
-func (l *NoopLogger) Infof(string, ...interface{})  {}
-func (l *NoopLogger) Warnf(string, ...interface{})  {}
-func (l *NoopLogger) Debugf(string, ...interface{}) {}
-func (l *NoopLogger) Fatalf(string, ...interface{}) {}
-func (l *NoopLogger) Error(...interface{})          {}
-func (l *NoopLogger) Info(...interface{})           {}
-func (l *NoopLogger) Warn(...interface{})           {}
-func (l *NoopLogger) Debug(...interface{})          {}
-func (l *NoopLogger) Fatal(...interface{})          {}
