4ca0d138e4eb850a5e10cf5ae1ea384035b105c8
Nikos <nikos.sklikas@canonical.com>
2024-03-29 14:15:29 +0200

    fix: update device_code page

--- ui/pages/device_code.tsx
+++ ui/pages/device_code.tsx
@@ -1,25 +1,73 @@
 import type { NextPage } from "next";
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { FormEvent, useCallback, useEffect } from "react";
 import PageLayout from "../components/PageLayout";
-import { Button, Input } from "@canonical/react-components";
+import { Button, Form, Input } from "@canonical/react-components";
+
+export interface Response {
+  data: {
+    redirect_to?: string;
+  };
+}
+
+function acceptUserCode(userCode: string, challenge: string) {
+  axios
+    .put("/api/device?device_challenge=" + String(challenge), {
+      user_code: userCode,
+    })
+    .then(({ data }: Response) => {
+      if (data.redirect_to) {
+        window.location.href = data.redirect_to;
+      }
+    })
+    .catch(() => {
+      return Promise.reject();
+    });
+  return;
+}
 
 const DeviceCode: NextPage = () => {
+  const router = useRouter();
+  const { device_challenge: challenge, user_code: code } = router.query;
+
+  useEffect(() => {
+    // If the router is not ready yet, or we already have a flow, do nothing.
+    if (!router.isReady) {
+      return;
+    }
+  }, [code]);
+
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const userCode = formData.get("code");
+      acceptUserCode(String(userCode), String(challenge));
+    },
+    [challenge],
+  );
+
   return (
     <PageLayout title="Enter code to continue">
-      <Input
-        id="code"
-        name="code"
-        type="text"
-        label="Terminal code"
-        placeholder="xxxx-xxxx"
-      />
-      <Button
-        type="submit"
-        appearance="positive"
-        className="u-no-margin--bottom"
-      >
-        Next
-      </Button>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          id="code"
+          name="code"
+          type="text"
+          placeholder="XXXXXXXX"
+          autoFocus={true}
+          defaultValue={code}
+        />
+        <Button
+          type="submit"
+          appearance="positive"
+          className="u-no-margin--bottom"
+        >
+          Next
+        </Button>
+      </Form>
     </PageLayout>
   );
 };
