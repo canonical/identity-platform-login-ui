c87b2909a0a2ff88de6ace5a5e87c305c3ae2abe
David Edler <david.edler@canonical.com>
2024-07-17 15:09:28 +0200

    fix: ui webauthn login to start automatically and not require user to click continue manually
    Signed-off-by: David Edler <david.edler@canonical.com>

--- ui/components/NodeInputButton.tsx
+++ ui/components/NodeInputButton.tsx
@@ -3,14 +3,18 @@ import { Button } from "@canonical/react-components";
 import React, { FC, FormEvent } from "react";
 import { NodeInputProps } from "./helpers";
 
-const getWebAuthnRegistrationPayload = (evalCode: string): unknown => {
-  const tmp = evalCode.replace("window.__oryWebAuthnRegistration(", "");
+const getWebAuthnPayload = (evalCode: string): unknown => {
+  const tmp = evalCode
+    .replace("window.__oryWebAuthnRegistration(", "")
+    .replace("window.__oryWebAuthnLogin(", "");
   const raw = tmp.substring(0, tmp.length - 1);
   return JSON.parse(raw) as unknown;
 };
 
 interface WebauthnWindow extends Window {
   __oryWebAuthnRegistration: (a: unknown) => void;
+  __oryWebAuthnLogin: (a: unknown) => void;
+  __oryStartedLogin: boolean;
 }
 
 export const NodeInputButton: FC<NodeInputProps> = ({
@@ -20,17 +24,45 @@ export const NodeInputButton: FC<NodeInputProps> = ({
   disabled,
   dispatchSubmit,
 }) => {
-  const handleClick = (e: MouseEvent | FormEvent) => {
-    const onClick = attributes.onclick;
+  const onClick = attributes.onclick ?? "";
 
-    // webauthn registration has a custom handler in webauthn.js to be called
+  // automatically start the webauthn login
+  const startWebAuthnLogin = () => {
+    const webauthnWindow = window as unknown as WebauthnWindow;
+    const loginParams = getWebAuthnPayload(onClick);
+
+    if (webauthnWindow.__oryStartedLogin) {
+      return;
+    }
+    webauthnWindow.__oryStartedLogin = true;
+
+    // retry login until the webauthn.js script is finished loading
+    // and the __oryWebAuthnLogin function is available
+    const triggerWebauthnLogin = (count: number) =>
+      setTimeout(() => {
+        if ("__oryWebAuthnLogin" in webauthnWindow) {
+          webauthnWindow.__oryWebAuthnLogin(loginParams);
+        } else if (count < 100) {
+          triggerWebauthnLogin(count + 1);
+        }
+      }, 100);
+
+    triggerWebauthnLogin(0);
+  };
+
+  if (onClick?.startsWith("window.__oryWebAuthnLogin(")) {
+    startWebAuthnLogin();
+  }
+
+  const handleClick = (e: MouseEvent | FormEvent) => {
+    // webauthn has a custom handler in webauthn.js to be called
     // preventing the call of eval with below code
     if (onClick?.startsWith("window.__oryWebAuthnRegistration(")) {
       e.stopPropagation();
       e.preventDefault();
 
       const webauthnWindow = window as unknown as WebauthnWindow;
-      const registrationParams = getWebAuthnRegistrationPayload(onClick);
+      const registrationParams = getWebAuthnPayload(onClick);
       webauthnWindow.__oryWebAuthnRegistration(registrationParams);
 
       return;
