a17d0aeb73352dc578a260d223abfac96b439aab
barco <marco.basile@canonical.com>
2024-08-06 09:42:31 +0200

    feat: implement MFA enforcing for getConsent
    this is to avoid a bug in which, if a user goes back
    in browser history without setting up TOTP method, he/she
    would be able to reach the target 3rd party app 100% authenticated
    without any issues. This prevents that.

--- pkg/extra/handlers.go
+++ pkg/extra/handlers.go
@@ -1,16 +1,30 @@
 package extra
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+
+	"github.com/go-chi/chi/v5"
+	client "github.com/ory/kratos-client-go"
 
 	"github.com/canonical/identity-platform-login-ui/internal/logging"
-	"github.com/go-chi/chi/v5"
+	"github.com/canonical/identity-platform-login-ui/pkg/kratos"
 )
 
 type API struct {
-	service ServiceInterface
+	mfaEnabled  bool
+	contextPath string
+	service     ServiceInterface
+	kratos      kratos.ServiceInterface
 
 	logger logging.LoggerInterface
+}
+
+type ConsentRedirectResponse struct {
+	RedirectTo string `json:"redirect_to,omitempty"`
 }
 
 func (a *API) RegisterEndpoints(mux *chi.Mux) {
@@ -19,7 +33,7 @@ func (a *API) RegisterEndpoints(mux *chi.Mux) {
 
 // TODO: Validate response when server error handling is implemented
 func (a *API) handleConsent(w http.ResponseWriter, r *http.Request) {
-	session, err := a.service.CheckSession(r.Context(), r.Cookies())
+	session, _, err := a.kratos.CheckSession(r.Context(), r.Cookies())
 
 	if err != nil {
 		a.logger.Errorf("error when calling kratos: %s", err)
@@ -36,6 +50,19 @@ func (a *API) handleConsent(w http.ResponseWriter, r *http.Request) {
 		a.logger.Errorf("error when calling hydra: %s", err)
 		// TODO @shipperizer evaluate return status
 		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	shouldEnforceMfa, err := a.shouldEnforceMFA(r.Context(), session)
+	if err != nil {
+		err = fmt.Errorf("enforce check error: %v", err)
+		a.logger.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if shouldEnforceMfa {
+		a.mfaSettingsRedirect(w)
 		return
 	}
 
@@ -57,12 +84,61 @@ func (a *API) handleConsent(w http.ResponseWriter, r *http.Request) {
 
 	w.Write(rr)
 	w.WriteHeader(http.StatusOK)
+
 }
 
-func NewAPI(service ServiceInterface, logger logging.LoggerInterface) *API {
+func (a *API) shouldEnforceMFA(ctx context.Context, session *client.Session) (bool, error) {
+	if !a.mfaEnabled {
+		return false, nil
+	}
+
+	// if using OIDC external provider, do not enforce MFA
+	for _, method := range session.AuthenticationMethods {
+		if method.Method != nil && *method.Method == "oidc" {
+			return false, nil
+		}
+	}
+
+	totpAvailable, err := a.kratos.HasTOTPAvailable(ctx, session.Identity.GetId())
+	if err != nil {
+		return false, err
+	}
+
+	return !totpAvailable, nil
+}
+
+func (a *API) mfaSettingsRedirect(w http.ResponseWriter) {
+	redirect, err := url.JoinPath("/", a.contextPath, "/ui/setup_secure")
+	if err != nil {
+		err = fmt.Errorf("unable to build mfa redirect path, possible misconfiguration, err: %v", err)
+		a.logger.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(
+		ConsentRedirectResponse{
+			RedirectTo: redirect,
+		},
+	)
+}
+
+func NewAPI(mfaEnabled bool, baseURL string, service ServiceInterface, kratos kratos.ServiceInterface, logger logging.LoggerInterface) *API {
 	a := new(API)
 
+	a.mfaEnabled = mfaEnabled
+
+	fullBaseURL, err := url.Parse(baseURL)
+	if err != nil {
+		// this should never happen if app is configured properly
+		a.logger.Fatalf("Failed to construct API base URL: %v\n", err)
+	}
+
+	a.contextPath = fullBaseURL.Path
+
 	a.service = service
+	a.kratos = kratos
 
 	a.logger = logger
 
