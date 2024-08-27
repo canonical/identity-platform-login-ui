eae7fa7303e0a78ed7b6d11d80e74f6d4f3220f9
David Edler <david.edler@canonical.com>
2024-08-27 10:11:22 +0200

    fix ui: regenerate backup codes screen to allow continue sign in process
    Signed-off-by: David Edler <david.edler@canonical.com>

--- ui/pages/backup_codes_regenerate.tsx
+++ ui/pages/backup_codes_regenerate.tsx
@@ -1,9 +1,35 @@
 import type { NextPage } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageLayout from "../components/PageLayout";
 import { Button } from "@canonical/react-components";
+import { useRouter } from "next/router";
+import { kratos } from "../api/kratos";
+import { handleFlowError } from "../util/handleFlowError";
+import { LoginFlow } from "@ory/client";
 
 const BackupCodesRegenerate: NextPage = () => {
+  const [flow, setFlow] = useState<LoginFlow>();
+
+  const router = useRouter();
+  const { flow: flowId } = router.query;
+
+  useEffect(() => {
+    // If the router is not ready yet, or we already have a flow, do nothing.
+    if (!router.isReady || flow) {
+      return;
+    }
+
+    if (flowId) {
+      kratos
+        .getLoginFlow({ id: String(flowId) })
+        .then((res) => setFlow(res.data))
+        .catch(handleFlowError("login", setFlow));
+      return;
+    }
+  }, [flowId, router, router.isReady, flow]);
+
+  const signInUrl = flow?.return_to ?? "./login";
+
   return (
     <PageLayout title="Backup code sign in successful">
       <p className="u-text--muted">
@@ -18,7 +44,7 @@ const BackupCodesRegenerate: NextPage = () => {
           Generate new codes
         </Button>
         <Button
-          onClick={() => (window.location.href = "./login")}
+          onClick={() => (window.location.href = signInUrl)}
           appearance="link"
         >
           I don&apos;t need new codes, sign in
