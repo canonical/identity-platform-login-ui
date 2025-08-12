70b45997bca992c9aed1183ce6bd372edbb0611c
shipperizer <alexcabb@gmail.com>
2025-08-12 12:29:33 +0200

    fix: deal with settings flow while setting up the passkeys, accept form data and redirect like kratos does

--- pkg/kratos/handlers.go
+++ pkg/kratos/handlers.go
@@ -775,6 +775,14 @@ func (a *API) handleUpdateSettingsFlow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// maintain previous kratos behaviour
+	if returnTo, ok := flow.GetReturnToOk(); ok {
+		a.redirectResponse(w, r, &BrowserLocationChangeRequired{
+			RedirectTo: returnTo,
+		})
+		return
+	}
+
 	resp, err := json.Marshal(flow)
 	if err != nil {
 		a.logger.Errorf("Error when marshalling json: %v\n", err)
