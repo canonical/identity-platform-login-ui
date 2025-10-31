c84c09db9f8d09085dc90c484561c3b0525fd4de
barco <marco.basile@canonical.com>
2025-10-31 16:19:06 +0100

    feat: prevent disabled pages to show up when visited explicitly

--- ui/pages/manage_secure.tsx
+++ ui/pages/manage_secure.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from "next";
 import React from "react";
 import SetupSecure from "./setup_secure";
+import { hasFeatureFlag, useAppConfig } from "../config/useAppConfig";
+import { notFound } from "next/navigation";
 
 const ManageSecure: NextPage = () => {
+  let appConfig = useAppConfig();
+  if (!hasFeatureFlag('totp', appConfig)) {
+    notFound()
+  }
+
   return <SetupSecure forceSelfServe={true} />;
 };
 
