042521bbeb94405bf7546b0dd313a1f87910f38f
Nikos Sklikas <nikos.sklikas@canonical.com>
2026-01-19 13:17:31 +0200

    chore: Remove screenshots from tests

--- ui/tests/helpers/password.ts
+++ ui/tests/helpers/password.ts
@@ -7,7 +7,6 @@ export const confirmMailCode = async (
   context: BrowserContext,
 ) => {
   await expect(page.getByText("Enter the code you received")).toBeVisible();
-  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 500 });
   const recoveryCode = await getRecoveryCodeFromMailSlurp(context);
   await page.getByLabel("Recovery code").fill(recoveryCode);
   await page.getByRole("button", { name: "Submit" }).click();
@@ -15,7 +14,6 @@ export const confirmMailCode = async (
 
 export const enterNewPassword = async (page: Page, password: string) => {
   await expect(page.getByText("Reset password").first()).toBeVisible();
-  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 500 });
   await page.getByLabel("New password", { exact: true }).fill(password);
   await page.getByLabel("Confirm New password").fill(password);
   await page.getByRole("button", { name: "Reset password" }).click();
--- ui/tests/helpers/totp.ts
+++ ui/tests/helpers/totp.ts
@@ -3,7 +3,6 @@ import { execSync } from "child_process";
 
 export const enterTotpCode = async (page: Page, setupKey: string) => {
   await expect(page.getByText("Verify your identity")).toBeVisible();
-  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 500 });
   const code = getTotpCode(setupKey);
   await page.getByLabel("Authentication code").fill(code);
   await page.getByRole("button", { name: "Sign in" }).click();
@@ -15,10 +14,6 @@ const getTotpCode = (setupKey: string) => {
 
 export const setupTotp = async (page: Page) => {
   await expect(page.getByText("Secure your account")).toBeVisible();
-  await expect(page).toHaveScreenshot({
-    fullPage: true,
-    maxDiffPixelRatio: 0.05,
-  }); // the code differs every time
   const setupKey = await getTotpSetupKey(page);
   const totpCode = getTotpCode(setupKey);
   await page.getByLabel("Verify code").fill(totpCode);
