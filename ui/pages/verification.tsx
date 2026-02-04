3ddc7088e8ea581d42b5c55b1b4ff2777322d8c6
Abhigyan <abhigyanghosh30@gmail.com>
2026-02-04 18:05:07 +0530

    fix(frontend): add user email var to the prompt on verification flow

--- ui/pages/verification.tsx
+++ ui/pages/verification.tsx
@@ -152,6 +152,16 @@ const Verification: NextPage = () => {
     [flow],
   );
 
+  const userEmail = useMemo(() => {
+    if (!flow) {
+      return "";
+    }
+    const emailNode = flow.ui.nodes.find(
+      (node) => (node.attributes as UiNodeInputAttributes).name === "email",
+    );
+    return emailNode ? (emailNode.attributes as UiNodeInputAttributes).value : "";
+  }, [flow]);
+
   const lookupFlow = useMemo(() => {
     if (!flow) {
       return flow;
@@ -169,7 +179,7 @@ const Verification: NextPage = () => {
             if (node.meta.label) {
               node.meta.label.context = {
                 ...node.meta.label.context,
-                beforeComponent: <EmailVerificationPrompt />,
+                beforeComponent: <EmailVerificationPrompt email={userEmail} />,
               };
             }
           }
