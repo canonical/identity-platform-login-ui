1510139d49603434b1480b3e4a8849e7d63090f5
Abhigyan <abhigyanghosh30@gmail.com>
2026-02-05 14:36:47 +0530

    fix(frontend): Resend code countdown not working on clicking the second time

--- ui/components/CountDownText.tsx
+++ ui/components/CountDownText.tsx
@@ -1,32 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 
 const CountDownText = ({
-  initialSeconds,
-  wrapperText,
+  wrapperText="",
+  initialSeconds=10,
 }: {
-  initialSeconds: number;
-  wrapperText: string;
+  wrapperText?: string;
+  initialSeconds?: number;
 }) => {
   const [seconds, setSeconds] = useState(initialSeconds);
   console.log("CountDownText rendered with seconds:", initialSeconds);
   useEffect(() => {
-    // 1. If the timer reaches 0, stop the interval
-    if (seconds <= 0) return;
-
-    // 2. Set up the interval
+    console.log("CountDownText useEffect triggered with seconds:", seconds);
     const timerId = setInterval(() => {
+      if (seconds <= 0){
+        clearInterval(timerId);
+        return;
+      };
       setSeconds((prev) => prev - 1);
       console.log("Timer tick:", seconds);
     }, 1000);
-
-    // 3. Cleanup: Clear interval if the component unmounts
-    // or before the effect runs again
     return () => clearInterval(timerId);
-  }, [initialSeconds]);
+  }, [initialSeconds, seconds]);
 
-  return seconds > 0
-    ? `${wrapperText}${seconds.toString().padStart(2, "0")}s`
-    : "";
+  if(seconds <= 0) {
+    return null;
+  }
+  return `${wrapperText}${seconds.toString().padStart(2, "0")}s`
 };
 
 export default CountDownText;
