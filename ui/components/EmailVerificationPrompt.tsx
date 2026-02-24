db4089606e7a18bdd2a39ed7e2ed7065bd7a9b6e
Abhigyan <abhigyanghosh30@gmail.com>
2026-02-25 01:01:32 +0530

    fix: fix spelling in email prompt

--- ui/components/EmailVerificationPrompt.tsx
+++ ui/components/EmailVerificationPrompt.tsx
@@ -6,7 +6,7 @@ export const EmailVerificationPrompt: React.FC<{ email: string }> = ({
   return (
     <p className="u-text--muted">
       An email with a verification code has been sent to {email}. Please check
-      your inbox and enter the code below. If the email is not recieved, ensure
+      your inbox and enter the code below. If the email is not received, ensure
       the address is correct.
     </p>
   );
